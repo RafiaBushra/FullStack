932e56d69fa3c0f8d2e240a0f5e56af42264facf
Rafiableh <saiyuriye.rafia@gmail.com>
2020-09-15 13:56:08 +0300

    Part 3c done!

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -6,11 +6,11 @@ import Notification from './components/Notifications'
 import shortcuts from './services/shortcuts'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filter, setFilter ] = useState('')
-  const [ notification, setNotification ] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState('')
 
   useEffect(() => {
     shortcuts.getAll()
@@ -20,45 +20,46 @@ const App = () => {
   }, [])
   // getAll sends axios.get() request to retrieve all the persons saved in the db.json file.
   const Persons = () => {
-    const filtered = !filter.length? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    const filtered = !filter.length ? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
     return (
       <div>
-        {filtered.map((person, id) => 
-          <Display key={id} 
-          person={person}
-          deleteHandler={deletePerson}/>
+        {filtered.map((person, id) =>
+          <Display key={id}
+            person={person}
+            deleteHandler={deletePerson} />
         )}
       </div>
     )
   } // Simply prints out the contacts in the phonebook.
   const addPerson = (event) => {
-    event.preventDefault()
+    // event.preventDefault()
+    // THIS IS A TEMPORARY FIX: THE ENTRY ID IS UNDEFINED UNTIL I REFRESH THE PAGE SO THIS IS HOW I "FIXED" IT.
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: persons[persons.length - 1].id + 1
+      number: newNumber
+      // id: persons[persons.length - 1].id + 1
       // new person's id would be the last person in the phonebook's id + 1
     }
     const duplicate = persons.find(person => person.name === personObject.name)
-    if (duplicate !== undefined) {
+    if (duplicate) {
       personObject.id = duplicate.id
       // Since we just want to update the contact info of the person, we give them the same id and only change their phone number.
       if (window.confirm(`${personObject.name} is already added to phonebook. Replace old number with the new one?`)) {
         shortcuts.update(duplicate.id, personObject).then(returnedPerson => {
-          setPersons(persons.map(person => 
-            person.id === duplicate.id? returnedPerson : person
-          )) 
+          setPersons(persons.map(person =>
+            person.id === duplicate.id ? returnedPerson : person
+          ))
         })
-        .catch(error => {
-          setNotification(
-            `Fail: the person '${personObject.name}' was already deleted from the phonebook.`
-          )
-          setTimeout(() => {
-            setNotification(null)
-          }, 5000)
-          return null
-        })
+          .catch(error => {
+            setNotification(
+              `Fail: the person '${personObject.name}' was already deleted from the phonebook.`
+            )
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000)
+            return null
+          })
         setNotification(
           `Success: updated ${personObject.name}'s number.`
         )
@@ -69,24 +70,25 @@ const App = () => {
     }
     else {
       shortcuts.create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(personObject))
-      })
+        .then(returnedPerson => {
+          setPersons(persons.concat(personObject))
+        })
       setNotification(
         `Success: added ${personObject.name} to phonebook.`
       )
       setTimeout(() => {
         setNotification(null)
       }, 5000)
+
     } // If it is a new contact then shortcuts.create() sends a axios.post request to server which adds the new contact to db.json. setPersons then adds the new contact to the end of the persons state array.
     setNewName('')
-    setNewNumber('')     
+    setNewNumber('')
   } // Checks the new entry does not already exist and adds it to database.
   const deletePerson = (id) => {
     const deleteThis = persons.find(p => p.id === id)
     // Finds the person object associated with the id in the parameter.
-    if (window.confirm(`Delete ${deleteThis.name}?`)){
-        shortcuts.remove(id)
+    if (window.confirm(`Delete ${deleteThis.name}?`)) {
+      shortcuts.remove(id)
         .then(() => {
           setPersons(persons.filter(person => person.id !== id))
         }) // shortcuts.remove() sends axios.delete() request to server and removes the entry associated with the id from db.json. setPersons() then updates the persons state so the deleted contact doesn't show up anymore. 
@@ -99,12 +101,12 @@ const App = () => {
           }, 5000)
           setPersons(persons.filter(p => p.id !== id))
         }) // Not really sure why this would be the case since every single delete button is associated with a contact that definitely still exists in the database, but oh well.
-        setNotification(
-          `Success: deleted ${deleteThis.name} from phonebook.`
-        )
-        setTimeout(() => {
-          setNotification(null)
-        }, 5000)
+      setNotification(
+        `Success: deleted ${deleteThis.name} from phonebook.`
+      )
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
     }
   }
   const handleNameChange = (event) => {
@@ -115,20 +117,18 @@ const App = () => {
   }
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
-  }  
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={notification}/>
+      <Notification message={notification} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Add new person</h3>
-      <PersonForm handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} addPerson={addPerson}/>
+      <PersonForm handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} newName={newName} newNumber={newNumber} addPerson={addPerson} />
       <h3>Numbers</h3>
       <Persons />
     </div>
   )
 }
-
-
 
 export default App
--- Part 3/index.js
+++ Part 3/index.js
@@ -1,100 +1,120 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(cors())
-
+app.use(express.static('build'))
 app.use(express.json())
-
-morgan.token('content', (req)=>JSON.stringify(req.body))
-
+// app.use(logger)
+morgan.token('content', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
+app.get('/api/people', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
+  })
+}) // Returns list of all the entries in the Phonebook database.
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      }
-  ]
-   
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  }) // Returns list of all the entries in persons[].
-
-  app.get('/info', (req, res) => {
+app.get('/info', (req, res) => {
+  Person.count({}, (error, c) => {
+    if(error) next(error)
     res.send(`
-    <div>Phonebook has info for ${persons.length} people.</div>
-    <div>${new Date()}</div>
-    `)
-  }) // Prints how many entries are in persons[] and exact time of sending the request to server.
+    <div>Phonebook has info for ${c} people.</div>
+    <div>${new Date()}</div>`)
+  })
+}) // Prints how many entries are in the databasse and the exact time of sending the request to server.
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }  
+app.get('/api/people/:id', (request, response) => {
+  //const id = Number(request.params.id)
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  }) // Prints the info of entry corresponding to the id. Error 404 if id doesn't exist.
+  }).catch(error => next(error))
+}) // Prints the info of entry corresponding to the id. Error 404 if id doesn't exist.
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)  
-    response.status(204).end()
-  }) // Deletes entry corresponding to the id.
+app.post('/api/people', (request, response) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name missing!'
+    })
+  }
+  else if (!body.number) {
+    return response.status(400).json({
+      error: 'Number missing!'
+    })
+  }
+  else {
+    Person.find({}).then(person => {
+      if (person.name === body.name) {
+        response.status(400).json({
+          error: 'Person with the same name already exists in the phonebook. Name for each entry must be unique.'
+        })
+      }
+    })
+  }
 
-  const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(100))
-  } // Generates a random ID ranging from 1 to 100 for new notes.
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'Name missing!' 
-      })
-    }
-    else if (!body.number) {
-        return response.status(400).json({
-            error: 'Number missing!'
-        })
-    }
-    else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'Person with the same name already exists in the phonebook. Name for each entry must be unique.'
-        })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: body.id,
   })
 
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  person.save().then(savedPerson => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook.`)
+    response.json(savedPerson)
   })
+
+}) // Checks the validity of the new entry and then saves the valid entry to database.
+
+app.delete('/api/people/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+}) // Deletes entry corresponding to the id.
+
+// const generateId = () => {
+//   return Math.floor(Math.random() * Math.floor(100))
+// } // Generates a random ID ranging from 1 to 100 for new notes.
+
+app.put('/api/people/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint.' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted ID.' })
+  }
+  else if (error.name === 'ReferenceError') {
+    return response.status(400).send({ error: 'A variable is not defined in the code.' })
+  }
+  next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
